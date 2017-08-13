3f1872156df3796d598c61ed2e76e8df39b04ad0
chuhan2014 <3228936343@qq.com>
2017-08-14 00:24:21 +0800

    first

---
+++ app/src/main/java/com/dingdangmao/tagview/Main.java
@@ -0,0 +1,15 @@
+package com.dingdangmao.tagview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+
+    }
+}
---
+++ app/src/main/java/com/dingdangmao/tagview/TagView.java
@@ -0,0 +1,140 @@
+package com.dingdangmao.tagview;
+
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by suxiaohui on 2017/8/13.
+ */
+
+public class TagView extends ViewGroup {
+
+    float bg_radius;
+    int bg_color;
+    int bg_alpha;
+    Paint p=new Paint();
+
+    public TagView(Context context) {
+        this(context,null);
+    }
+
+    public TagView(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public TagView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        TypedArray attr = context.obtainStyledAttributes(attrs, R.styleable.TagView, defStyleAttr, 0);
+
+        try {
+
+            bg_radius = attr.getDimension(R.styleable.TagView_bg_radius,5);
+            bg_alpha = attr.getInteger(R.styleable.TagView_bg_alpha,100);
+            bg_color = attr.getColor(R.styleable.TagView_bg_color,Color.parseColor("#FFCCCC"));
+
+
+        } catch (Exception e) {
+
+            Log.i("Unit", e.toString());
+
+        } finally {
+
+            attr.recycle();
+        }
+        p = new Paint();
+        p.setStyle(Paint.Style.FILL);
+        p.setColor(bg_color);
+        p.setAlpha(bg_alpha);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
+
+        int num = getChildCount();
+        int lPadding = getPaddingLeft();
+        int rPadding = getPaddingRight();
+        int tPadding = getPaddingTop();
+        int bPadding = getPaddingBottom();
+
+        int height = tPadding + bPadding;
+        int width = lPadding + rPadding;
+
+        for (int i = 0; i < num; i++) {
+            View c = getChildAt(i);
+
+            MarginLayoutParams mp = (MarginLayoutParams) c.getLayoutParams();
+            if (width + c.getMeasuredWidth() + mp.leftMargin + mp.rightMargin > widthSize) {
+                width = lPadding + rPadding + c.getMeasuredWidth() + mp.leftMargin + mp.rightMargin;
+                if (height + c.getHeight() + mp.topMargin + mp.bottomMargin > heightSize)
+                    break;
+                height += c.getHeight() + mp.topMargin + mp.bottomMargin;
+            } else {
+                width += c.getMeasuredWidth() + mp.leftMargin + mp.rightMargin;
+            }
+        }
+        setMeasuredDimension((widthMode == MeasureSpec.EXACTLY) ? widthSize
+                : width, (heightMode == MeasureSpec.EXACTLY) ? heightSize
+                : height);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+        int num = getChildCount();
+        int lPadding = getPaddingLeft();
+        int rPadding = getPaddingRight();
+        int tPadding = getPaddingTop();
+        int bPadding = getPaddingBottom();
+
+        int cw = lPadding;
+        int ch = tPadding;
+        int right = getWidth() - rPadding;
+        int bottom = getHeight() - bPadding;
+
+        for (int i = 0; i < num; i++) {
+            View c = getChildAt(i);
+            MarginLayoutParams mp = (MarginLayoutParams) c.getLayoutParams();
+            if (cw + c.getMeasuredWidth() + mp.leftMargin + mp.rightMargin > right) {
+                if (ch + c.getMeasuredHeight() + mp.topMargin + mp.bottomMargin > bottom)
+                    break;
+                cw = lPadding + mp.leftMargin + c.getMeasuredWidth();
+                ch += mp.topMargin + mp.bottomMargin + c.getMeasuredHeight();
+                c.layout(lPadding + mp.leftMargin, ch + mp.topMargin, cw, ch + mp.topMargin + c.getMeasuredHeight());
+                cw += mp.rightMargin;
+            } else {
+                c.layout(cw + mp.leftMargin, ch + mp.topMargin, cw + mp.leftMargin + c.getMeasuredWidth(), ch + c.getMeasuredHeight() + mp.topMargin);
+                cw = cw + mp.leftMargin + mp.rightMargin + c.getMeasuredWidth();
+                Log.i("Unit", String.valueOf(c.getMeasuredHeight()));
+                Log.i("Unit", String.valueOf(ch + c.getMeasuredHeight() + mp.topMargin + mp.bottomMargin));
+            }
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawRect(0,0,getWidth(),getHeight(),p);
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+
+        return new MarginLayoutParams(getContext(), attrs);
+
+    }
+}
